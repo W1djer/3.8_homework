f987e4e066737c2f8da862f1cd7e3a2ce8f83351
Evgenii <w1djer@mail.ru>
2023-04-02 19:18:21 +0300

    Задача доп

---
+++ доп_задача/Program.cs
@@ -0,0 +1,19 @@
+﻿int Input()
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int Process(int number)
+{
+    int i = 1;
+    int m = 1;
+    while (i <= number)
+    {
+        m++;
+        i = m*m;
+    }
+    return m-1;
+}
+Console.Clear();
+Console.WriteLine($"Целая часть: {Process(Input())}");
